7e97eb45be43ff8b7d42463cffac5bcc26f19c8c
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-12 18:58:03 -0300

    Feat: Testa Conexão

---
+++ server/src/config/config.js
@@ -0,0 +1,14 @@
+const config = {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql',
+};
+
+module.exports = {
+    development: config,
+    test: config,
+    production: config,
+};
