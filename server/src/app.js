82e1867e77dd465a1ebacbf9f15d3713a66b514a
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-12 19:26:30 -0300

    Feat: Instala e Configura cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
