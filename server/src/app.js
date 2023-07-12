7650d42ea68412c7af72dceb7beb7c2632347138
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-12 18:50:34 -0300

    Feat: testa conexão

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(200).json({ message: 'Hello World!' });
 });
 
 module.exports = app;
