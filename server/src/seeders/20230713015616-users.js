5451242fc5182d6b50229e0d744c3992db6fdfe0
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-14 18:44:03 -0300

    Feat: Retira código para resetar id

--- server/src/seeders/20230713015616-users.js
+++ server/src/seeders/20230713015616-users.js
@@ -5,11 +5,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface) {
-        await queryInterface.sequelize.query('ALTER TABLE users AUTO_INCREMENT = 1;');
         return queryInterface.bulkInsert('users', [
             {
                 full_name: 'Ruan Portella',
                 email: 'ruanmorales29@gmail.com',
+                password: 'dasidnasudausn',
+            },
+            {
+                full_name: 'Ruan Portella',
+                email: 'ruanportella.dev@gmail.com',
                 password: 'dasidnasudausn',
             },
         ], {});
