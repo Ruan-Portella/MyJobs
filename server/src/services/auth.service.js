f33185fe961f323546343af1721b9afd45ba1211
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-14 18:43:45 -0300

    Feat: Adiciona principio de SOLID no service

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
+const Secret = process.env.JWT_SECRET || 'secret';
+
 const signUpUser = async (fullName, email, password) => {
     try {
         await User.create({ fullName, email, password });
@@ -11,18 +13,24 @@ const signUpUser = async (fullName, email, password) => {
     }
 };
 
+const findUserByEmail = async (email) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw new Error('User not found');
+    return user;
+};
+
+const validatePassword = async (password, user) => {
+    const passwordValidate = await bcrypt.compare(password, user.password);
+    if (!passwordValidate) throw new Error('Wrong password');
+    return passwordValidate;
+};
+
 const signInUser = async (email, password) => {
     try {
-        const user = await User.findOne({ where: { email } });
+        const user = await findUserByEmail(email);
+        await validatePassword(password, user);
 
-        if (!user) return { message: 'User not found' };
-
-        const passwordValidate = await bcrypt.compare(password, user.password);
-        if (!passwordValidate) return { message: 'Wrong password' };
-
-        const Secret = process.env.JWT_SECRET || 'secret';
-
-        const token = jwt.sign({ id: user.id, name: user.fullName }, Secret, { algorithm: 'HS256', expiresIn: '600s' });
+        const token = jwt.sign({ id: user.id, name: user.fullName }, Secret, { algorithm: 'HS256', expiresIn: '300s' });
 
         return { name: user.fullName, token };
     } catch (error) {
