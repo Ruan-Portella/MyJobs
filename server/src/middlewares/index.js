44f5cc904efe180762100ff08ddf018bab0a7162
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-14 18:58:42 -0300

    Feat: Coloca a validação no index de middlewares

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,9 +1,11 @@
 const validateName = require('./validateName');
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const validateToken = require('./validateToken');
 
 module.exports = {
     validateName,
     validateEmail,
     validatePassword,
+    validateToken,
 };
