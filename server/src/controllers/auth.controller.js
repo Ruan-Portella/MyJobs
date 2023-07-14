2d92e2fc8c6dd25da8ebc6c7b76e0f8ab51d457f
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-14 16:53:21 -0300

    Feat: Cria função de logar e criar usuario na controller

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+const authService = require('../services/auth.service');
+
+const signUpUser = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const response = await authService.signUpUser(fullName, email, passwordHash);
+    if (response.message) return res.status(400).json({ message: response.message });
+    return res.status(201).json({ message: response });
+};
+
+const signInUser = async (req, res) => {
+    const { email, password } = req.body;
+    const response = await authService.signInUser(email, password);
+    if (response.message) return res.status(400).json({ message: response.message });
+    return res.status(200).json(response);
+};
+
+module.exports = {
+    signUpUser,
+    signInUser,
+};
