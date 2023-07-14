14cc7b953ff1889ed70113f0dd62f860ea9836d8
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-14 16:57:05 -0300

    Feat: Cria index para importação de middlewares

---
+++ server/src/middlewares/index.js
@@ -0,0 +1,9 @@
+const validateName = require('./validateName');
+const validateEmail = require('./validateEmail');
+const validatePassword = require('./validatePassword');
+
+module.exports = {
+    validateName,
+    validateEmail,
+    validatePassword,
+};
