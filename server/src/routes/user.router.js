551a5c9370a3815812538d218937a7188d851d9d
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-15 15:51:23 -0300

    Feat: Cria função para fazer o update de um job

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user.controller');
 
 userRouter.get('/', validations.validateToken, userController.getAllUserJobs);
 userRouter.post('/', validations.validateToken, userController.createJob);
+userRouter.put('/', validations.validateToken, userController.updateJob);
 
 module.exports = userRouter;
