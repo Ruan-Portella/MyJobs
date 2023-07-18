538834c007a40a63688e249427c58b9e7ba0073e
Ruan-Portella <ruanmorales29@gmail.com>
2023-07-18 19:32:16 -0300

    Feat: coloca googleId no seeder

--- server/src/seeders/20230713015616-users.js
+++ server/src/seeders/20230713015616-users.js
@@ -7,11 +7,13 @@ module.exports = {
     async up(queryInterface) {
         return queryInterface.bulkInsert('users', [
             {
+                google_id: '1176dasdasdad31468000000',
                 full_name: 'Ruan Portella',
                 email: 'ruanmorales29@gmail.com',
                 password: '$2b$10$MqJZiliUM7zVxJL6zNkGfODbD5LnX7f0MQZsXdta0./dPzmdikJPm',
             },
             {
+                google_id: '1176dasdasad3331468000000',
                 full_name: 'Ruan Portella',
                 email: 'ruanportella.dev@gmail.com',
                 password: '$2b$10$MqJZiliUM7zVxJL6zNkGfODbD5LnX7f0MQZsXdta0./dPzmdikJPm',
